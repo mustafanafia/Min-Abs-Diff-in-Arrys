30856bc36dc89b25dba4e27fcde7d5ad1948889d
mustafanafia <55895079+mustafanafia@users.noreply.github.com>
2024-03-19 14:08:17 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'minimumAbsoluteDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int minimumAbsoluteDifference(List<Integer> arr) {
+        // Write your code here
+
+        Collections.sort(arr);
+        int min_diff = Integer.MAX_VALUE;
+
+
+        for(int i=0; i<arr.size() - 1; i++){
+
+            int current_max_diff = Math.abs(arr.get(i) - arr.get(i+1));
+
+            min_diff = Math.min(min_diff, current_max_diff);
+
+        }
+
+        return min_diff;
+
+
+
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        int result = Result.minimumAbsoluteDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
